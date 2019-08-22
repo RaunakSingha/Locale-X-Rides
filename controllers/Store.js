ff30430b18a034f72eed46db92c6ba03abfc2b3b
Raunak Singha <rksingha1212@gmail.com>
2019-08-22 20:59:57 +0530

    Added storage api with authentication

---
+++ controllers/Store.js
@@ -0,0 +1,49 @@
+import db from '../db';
+
+
+
+
+const Store = {
+
+    async saveRideDetails(req, res) {
+
+        try {
+            const q = `INSERT INTO store(id, user_id, vehicle_model_id, package_id,
+                travel_type_id, from_area_id, to_area_id, from_city_id, to_city_id,
+                from_date, to_date, online_booking, mobile_site_booking, booking_created, from_lat, from_long,
+                to_lat, to_long, Car_Cancellation) 
+                VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19) returning *`
+
+            const values = [
+                req.body.id,
+                req.body.user_id,
+                req.body.vehicle_model_id,
+                req.body.package_id,
+                req.body.travel_type_id,
+                req.body.from_area_id,
+                req.body.to_area_id,
+                req.body.from_city_id,
+                req.body.to_city_id,
+                req.body.from_date,
+                req.body.to_date,
+                req.body.online_booking,
+                req.body.mobile_site_booking,
+                req.body.booking_created,
+                req.body.from_lat,
+                req.body.from_long,
+                req.body.to_lat,
+                req.body.to_long,
+                req.body.Car_Cancellation
+            ];
+
+            const { rows } = await db.query(q, values);
+            return res.status(200).send({ 'message': "Data registered successfully" });
+        }
+        catch (error) {
+            return res.status(400).send(error);
+        }
+    },
+
+}
+
+export default Store;
--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-import '@babel/polyfill';
+import User from './controllers/User';
+import Store from './controllers/Store';
+import Auth from './middleware/Auth'
 
+import '@babel/polyfill';
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +17,9 @@ app.get('/', (req, res) => {
     return res.status(200).send({ 'message': 'XRides Unofficial!' });
 });
 
+app.post('/user/signup', User.signUp);
+app.post('/user/login', User.signIn);
+app.post('/user/store', Auth.verifyToken, Store.saveRideDetails);
 
 
 app.listen(PORT, () => {
